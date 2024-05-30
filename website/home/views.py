9f06caaf961239235b1f077d92be14a41d8e40eb
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2024-05-30 11:26:30 +0100

    Update views.py

--- website/home/views.py
+++ website/home/views.py
@@ -3,10 +3,11 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
-@login_required(login_url='home/login/')
+
 def login(request):
      return render(request, 'registration/login.html', {'section':'main'})
 
+@login_required
 def mainpage(request):
     return render(request, 'registration/mainpage.html')
 
