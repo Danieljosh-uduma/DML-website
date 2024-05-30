8a17714980bc9120c61261be6e14ea0ac8595056
Daniel Josh Uduma <116913814+Danieljosh-uduma@users.noreply.github.com>
2024-05-30 11:26:38 +0100

    Update urls.py

--- website/home/urls.py
+++ website/home/urls.py
@@ -6,8 +6,8 @@ from django.contrib.auth import views as auth_views
 app_name = 'home'
 
 urlpatterns = [
-    path('homepage/', views.homepage, name='homepage'),
-    path('', views.mainpage, name='mainpage'),
+    path('', views.homepage, name='homepage'),
+    path('homepage/', views.mainpage, name='mainpage'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
